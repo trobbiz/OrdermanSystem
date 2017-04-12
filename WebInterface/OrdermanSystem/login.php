5550e76a8af0214dd24b733aeefcaf108b988fa8
fxab <felix.aberham1@gmail.com>
2017-04-12 13:02:00 +0200

    Add files via upload

---
+++ WebInterface/OrdermanSystem/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+		<title>Login Orderman</title>
+
+		<meta charset="utf-8" />
+		<meta name="content-language"	content="de" />
+		<meta name="author" 			content="felux" />
+		<meta name="description" 	    content="Login Orderman" />
+		<meta name="language"         	content="Deutsch" />
+
+		<link rel="stylesheet" href="stylesheet.css">
+	</head>
+    <body>
+	
+    <a href="http://www.html-seminar.de/metatag-wirkung.htm">
+	Auswirkungen der Meta-Tags</a>
+   
+    </body>
+</html>
